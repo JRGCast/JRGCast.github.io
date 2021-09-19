afd83fa747480f452fd999c8cdc21854944bb003
JRGCast <joao.ralph1@gmail.com>
2021-09-19 18:47:21 -0300

    adicionado loading ao projects, com respectiva logica

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
 import ProjectCard from "../components/ProjectCard";
 import { frontProjs } from "../projects/projectsList";
 import "./Projects.css";
 const Projects = () => {
   const [indexFrontProjs, setIndexFrontProjs] = useState(0);
+  const [allProjs, setAllProjs] = useState([]);
+  const [projsLoaded, setProjsLoaded] = useState(false);
   const nextFProj = () => {
-    indexFrontProjs < frontProjs.length - 1 ? setIndexFrontProjs(indexFrontProjs + 1) : setIndexFrontProjs(0);
-    console.log(mappingProjectList(frontProjs)[indexFrontProjs].props);
+    indexFrontProjs < allProjs.length - 1 ? setIndexFrontProjs(indexFrontProjs + 1) : setIndexFrontProjs(0);
   };
   const prevFProj = () => {
     indexFrontProjs > 0 ? setIndexFrontProjs(indexFrontProjs - 1) : setIndexFrontProjs(frontProjs.length - 1);
-    console.log(mappingProjectList(frontProjs)[indexFrontProjs].props);
   };
 
   const mappingProjectList = (projectList) => projectList.map(({ title, summary, stack, description, imgSrc, site, repo }) => {
@@ -23,23 +24,33 @@ const Projects = () => {
       site={ site }
       repo={ repo } />;
   });
+
+  useEffect(() => {
+    console.log(allProjs);
+    setAllProjs(frontProjs);
+    (setProjsLoaded(true));
+  }, [allProjs]);
+
   return (
     <section>
-      <h1>Meus projetos</h1>
-      <main>
-        <section style={ { marginBottom: "20px" } }>
-          <div className="Projects-projects-container">
-            <header className="Projects-button-container">
-              <button className="Projects-previous-button" type="button" onClick={ prevFProj }>{ '<<' }</button>
-              <h1 className="Projects-projects-title">{ mappingProjectList(frontProjs)[indexFrontProjs].props.title }</h1>
-              <button className="Projects-next-button" type="button" onClick={ nextFProj }>{ '>>' }</button>
-            </header>
-            <div>
-              { mappingProjectList(frontProjs)[indexFrontProjs] }
-            </div>
-          </div>
-        </section>
-      </main>
+      { projsLoaded ?
+        <>
+          <h1> Meus projetos</h1>
+          <main>
+            <section style={ { marginBottom: "20px" } }>
+              <div className="Projects-projects-container">
+                <header className="Projects-button-container">
+                  <button className="Projects-previous-button" type="button" onClick={ prevFProj }>{ '<<' }</button>
+                  <h1 className="Projects-projects-title">{ mappingProjectList(allProjs)[indexFrontProjs].props.title }</h1>
+                  <button className="Projects-next-button" type="button" onClick={ nextFProj }>{ '>>' }</button>
+                </header>
+                <div>
+                  { mappingProjectList(allProjs)[indexFrontProjs] }
+                </div>
+              </div>
+            </section>
+          </main>
+        </> : <Loading /> }
     </section>
   );
 };
