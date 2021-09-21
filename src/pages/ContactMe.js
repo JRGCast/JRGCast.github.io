2e02581f83ad332c3b0c6fd6bb572d57a2a76576
JRGCast <joao.ralph1@gmail.com>
2021-09-21 14:06:06 -0300

    adicionado badge linkedin

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -3,6 +3,7 @@ const ContactMe = () => {
     <main>
       <p>Tel: (61)98242-3750</p>
       <p>Email: joao.ralph1@gmail.com</p>
+      <div class="badge-base LI-profile-badge" data-locale="pt_BR" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="joao-ralph-castaldi" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/joao-ralph-castaldi?trk=profile-badge">João Ralph Castaldi</a></div>
     </main>);
 };
 
