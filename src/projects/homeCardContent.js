e0f495c450ccc1aa25f712953032d41e5f8e8559
JRGCast <joao.ralph1@gmail.com>
2021-09-21 18:35:05 -0300

    fixed Meus Projetos direcionando a Contatos

--- src/projects/homeCardContent.js
+++ src/projects/homeCardContent.js
@@ -2,7 +2,7 @@ const generateObj = (title, summary, image = '', imageAlt = '', goTo) => {
   return ({ title, summary, image, imageAlt, goTo });
 };
 
-const myProjsPage = generateObj('Meus Projetos', 'Contendo meus projetosaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'Meus Projetos foto', 'Meus Projetos alt', '/contactme');
+const myProjsPage = generateObj('Meus Projetos', 'Contendo meus projetosaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'Meus Projetos foto', 'Meus Projetos alt', '/projects');
 const aboutPage = generateObj('Sobre Mim', 'Bio', 'Bio Foto', 'Bio foto Alt', '/about');
 const contactPage = generateObj('Contate-me', 'Contato', 'ContFoto', 'ContAlt', '/contactme');
 
